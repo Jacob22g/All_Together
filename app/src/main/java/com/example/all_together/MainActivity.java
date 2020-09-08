f3adc8a64499977639c812595f49250469630063
Jacob <jacob.22.g@gmail.com>
2020-09-08 14:46:50 +0300

    add register fragment but the card view blocks it.

--- app/src/main/java/com/example/all_together/MainActivity.java
+++ app/src/main/java/com/example/all_together/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -13,9 +16,13 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RegisterFragment.OnRegisterFragmentListener {
+
+    final String REGISTER_FRAGMENT_TAG = "register_fragment";
 
     DrawerLayout drawerLayout;
     private Toolbar toolbar;
@@ -31,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
 //        ActionBar actionBar = getSupportActionBar();
 //        actionBar.setDisplayHomeAsUpEnabled(true);
 //        actionBar.setHomeAsUpIndicator(R.drawable.home_icon2);
+
+        Button registerBtn = findViewById(R.id.regiser_btn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RegisterFragment registerFragment = new RegisterFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.add(R.id.root_layout, registerFragment,REGISTER_FRAGMENT_TAG);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
     }
 
     @Override
@@ -53,5 +75,13 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    @Override
+    public void onRegister(String username, String password, String email) {
+
+        Toast.makeText(this, username + ", " + password + ", " + email, Toast.LENGTH_SHORT).show();
+        Fragment fragment = getSupportFragmentManager().findFragmentByTag(REGISTER_FRAGMENT_TAG);
+        getSupportFragmentManager().beginTransaction().remove(fragment).commit();
     }
 }
