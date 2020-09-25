8dbec11a0e078b8afff5c629c3a6cec2fa5d6fc4
Jacob <jacob.22.g@gmail.com>
2020-09-25 16:18:56 +0300

    added some ui chats and chat adapter

---
+++ app/src/main/java/com/example/all_together/model/ChatMessage.java
@@ -0,0 +1,48 @@
+package com.example.all_together.model;
+
+public class ChatMessage {
+
+    private String messageText;
+    private String sender;
+    private String receiver;
+    private long messageTime;
+
+    public ChatMessage(String messageText, String sender, String receiver, long messageTime) {
+        this.messageText = messageText;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messageTime = messageTime;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
---
+++ app/src/main/java/com/example/all_together/ui/chat/ConversationChatFragment.java
@@ -0,0 +1,26 @@
+package com.example.all_together.ui.chat;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.all_together.R;
+
+class ConversationChatFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_conversation_chat, container, false);
+
+
+
+        return view;
+    }
+}
