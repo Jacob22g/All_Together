204c13813a25f3112941cdbc8ccba28cdc9980b8
Lidan Nissim <nissimlidan@gmail.com>
2020-10-02 16:02:51 +0300

    add_design

--- app/src/main/java/com/example/all_together/FragmentVerifyPhoneNumber.java
+++ app/src/main/java/com/example/all_together/FragmentVerifyPhoneNumber.java
@@ -130,7 +130,7 @@ public class FragmentVerifyPhoneNumber extends Fragment {
                         if(task.isSuccessful()){
 //                                    FirebaseUser user = task.getResult().getUser();
                             Toast.makeText(getContext(), "Goooood", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getContext(),MainAppActivity.class);
+                            Intent intent = new Intent(getContext(),OldUserActivity.class);
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                             startActivity(intent);
 
